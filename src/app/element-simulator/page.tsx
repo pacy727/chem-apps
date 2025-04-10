7674c2da78be480b587e931d44f324d1cd7aba5b
pacy <pacy727@gmail.com>
2025-04-10 15:48:05 +0900

    初回コミット

---
+++ src/app/element-simulator/page.tsx
@@ -0,0 +1,11 @@
+// src/app/element-simulator/page.tsx
+'use client';
+export default function Game1Page() {
+  return (
+    <iframe
+      src="/element-simulator/index.html"
+      className="w-full h-screen border-0"
+      title="element-simulator"
+    ></iframe>
+  );
+}
